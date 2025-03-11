b8f8a9f24f3746c71ce49677f75e96702b0c6584
BUI QUOC VAN <116750804+bbqqvv@users.noreply.github.com>
2025-03-12 00:42:33 +0700

    _1148_Leetcode_ArticleViewI
    SQL50

---
+++ src/SQL50/Select/_1148_Leetcode_ArticleViewI.sql
@@ -0,0 +1,20 @@
+
+/**
+ * _01_Leetcode75_MergeStringAlternately
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date: 3/12/2025
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 3/12/2025    VANQUOCBUI    Create
+ */
+
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id;
