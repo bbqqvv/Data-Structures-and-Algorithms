5281c06bae520a48587e7692e925ee42707c42c6
BUI QUOC VAN <116750804+bbqqvv@users.noreply.github.com>
2025-03-12 00:58:22 +0700

    _1683_Leetcode_InvalidTweets
    SQL50

---
+++ src/SQL50/Select/_1683_Leetcode_InvalidTweets.sql
@@ -0,0 +1,25 @@
+/**
+ * _01_Leetcode75_MergeStringAlternately
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date: 3/12/2025
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 3/12/2025    VANQUOCBUI    Create
+ */
+ 
+SELECT tweet_id
+FROM Tweets
+WHERE CHAR_LENGTH(content) > 15;
+
+-- CHAR_LENGTH(content): Đếm số ký tự trong chuỗi (tốt hơn nếu có Unicode).
+-- LENGTH(content): Đếm số byte (có thể khác nếu chuỗi chứa ký tự đa byte).
+-- C2:
+-- SELECT tweet_id
+-- FROM Tweets
+-- WHERE LENGTH(content) > 15;
