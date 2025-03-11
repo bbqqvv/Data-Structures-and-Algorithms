71bcc27c5b088ae6f79fe77e95780110a2aa2a90
BUI QUOC VAN <116750804+bbqqvv@users.noreply.github.com>
2025-03-11 22:03:26 +0700

    _01_Leecode_TwoSum

---
+++ src/algorithms/_01_Leecode_TwoSum.java
@@ -0,0 +1,33 @@
+package algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TwosSum
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date: 3/11/2025
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 3/11/2025    VANQUOCBUI   Create
+ */
+
+class _01_Leecode_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
