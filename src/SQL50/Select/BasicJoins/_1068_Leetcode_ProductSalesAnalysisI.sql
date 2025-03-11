301d2f57fcd925991acb691335eea7cc0f85f075
BUI QUOC VAN <116750804+bbqqvv@users.noreply.github.com>
2025-03-12 01:55:33 +0700

    Update _1068_Leetcode_ProductSalesAnalysisI.sql

--- src/SQL50/Select/BasicJoins/_1068_Leetcode_ProductSalesAnalysisI.sql
+++ src/SQL50/Select/BasicJoins/_1068_Leetcode_ProductSalesAnalysisI.sql
@@ -1,2 +1,17 @@
+
+/**
+ * _01_Leetcode75_MergeStringAlternately
+ * <p>
+ * Version 1.0
+ * <p>
+ * Date: 3/12/2025
+ * <p>
+ * Copyright
+ * <p>
+ * Modification Logs:
+ * DATE        AUTHOR        DESCRIPTION
+ * --------------------------------------
+ * 3/12/2025    VANQUOCBUI    Create
+ */
 # Write your MySQL query statement below
 select year, price, product_name from Sales INNER JOIN Product ON Sales.product_id = Product.product_id
